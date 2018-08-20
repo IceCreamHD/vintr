ead89b69ee5834caaec14c8fa51438879efa8e94
IceCreamHD <dev1ce@icloud.com>
2018-08-20 20:11:32 +0300

    Add files via upload

---
+++ js/scr.js
@@ -0,0 +1,7 @@
+var promoButton = document.getElementById('vi-button-promo');
+promoButton.addEventListener('click', createElement);
+
+function createElement() {
+  var div = document.createElement('div');
+  div.textContent = null;
+}
